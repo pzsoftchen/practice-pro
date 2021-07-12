e822fea3c967040b97f077c1713875222918067d
pengzhenchen <pzsoftchen@163.com>
2021-07-12 10:49:10 +0800

    feat: 面试编程记录

---
+++ java-pro/src/main/java/com/chen/java/interview/tx/RedGift.java
@@ -0,0 +1,43 @@
+package com.chen.java.interview.tx;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * <p>
+ *  春节期间小明使用微信收到很多个红包，非常开心。在查看领取红包记录时发现，某个红包金额出现的次数超过了红包总数的一半。请帮小明找到该红包金额。写出具体算法思路和代码实现，要求算法尽可能高效。
+ *  给定一个红包的金额数组gifts及它的大小n，请返回所求红包的金额。
+ *  若没有金额超过总数的一半，返回0。
+ *  测试：
+ *  [1,2,3,2,2],5
+ *  结果：
+ *  2
+ * </p>
+ *
+ * @author pengzhenchen 2021/06/19 10:20 下午
+ */
+public class RedGift {
+
+    public static void main(String[] args) {
+        System.out.println(gift(new int[]{1,2,3,2,2},5));
+    }
+
+    private static int gift(int[] gifts, int len){
+
+        if (gifts.length == len) {
+            Map<Integer, Integer> count = new HashMap<>(len);
+            for (int i = 0; i < len; i++) {
+                if (count.containsKey(gifts[i])) {
+                    count.put(gifts[i], count.get(gifts[i]) + 1);
+                } else {
+                    count.put(gifts[i], 1);
+                }
+                if (count.get(gifts[i]) > len / 2) {
+                    return gifts[i];
+                }
+            }
+        }
+        return 0;
+    }
+}
+
---
+++ java-pro/src/main/java/com/chen/java/thread/MyRunnable.java
@@ -0,0 +1,40 @@
+package com.chen.java.thread;
+
+import java.util.Date;
+
+/**
+ * @program: practice-pro
+ * @class：MyRunnable
+ * @description: myrunnable
+ * @author: pzsoftchen
+ * @create: 06/29/2020 16:50:02
+ **/
+public class MyRunnable implements Runnable{
+
+
+    private String command;
+
+    public MyRunnable(String s) {
+        this.command = s;
+    }
+
+    @Override
+    public void run() {
+        System.out.println(Thread.currentThread().getName() + " Start. Time = " + new Date());
+        processCommand();
+        System.out.println(Thread.currentThread().getName() + " End. Time = " + new Date());
+    }
+
+    private void processCommand() {
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public String toString() {
+        return this.command;
+    }
+}
